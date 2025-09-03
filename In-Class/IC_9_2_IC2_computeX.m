a477d3c0c7b450188ed440f1e653627cc2c008e3
Caleb Bahne <calebbahne@gmail.com>
2025-09-03 12:43:43 -0700

    Homework 2 worked on, IC added

---
+++ In-Class/IC_9_2_IC2_computeX.m
@@ -0,0 +1,7 @@
+%% In-Class 2 - Caleb Bahne
+
+% Write a MATLAB expression to compute and display the values of a vector
+% x using the following equation. Assume that y and z are vector 
+% quantities of equal length and a and b are scalars.
+
+x = y.*(a+b*z).^1.8./(z.*(1-y));
