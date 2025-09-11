bee460f5b356acbc1957295204010e2ddbf79d6d
Caleb Bahne <calebbahne@gmail.com>
2025-09-10 17:51:37 -0700

    Started HW3, buggy open it

--- Ch3 - Programming with MATLAB/phasor.m
+++ Ch3 - Programming with MATLAB/phasor.m
@@ -3,20 +3,21 @@ function phasor(r, nt, nm)
 % r = radius
 % nt = number of increments for theta
 % nm = number of movies
-clc;clf
+clc;clf;
+
 dtheta=2*pi/nt;
 th=0;
 fac=1.2;
 xx=r;yy=0;
 for i=1:nt+1
-x=r*cos(th);y=r*sin(th);
-xx=[xx x];yy=[yy y];
-plot([0 x],[0 y],xx,yy,':',...
-x,y,'o','MarkerFaceColor','b','MarkerSize',8)
-axis([-fac*r fac*r -fac*r fac*r]);
-axis square
-M(i)=getframe;
-th=th+dtheta;
+    x=r*cos(th);y=r*sin(th);
+    xx=[xx x];yy=[yy y];
+    plot([0 x],[0 y],xx,yy,':',...
+    x,y,'o','MarkerFaceColor','b','MarkerSize',8)
+    axis([-fac*r fac*r -fac*r fac*r]);
+    axis square
+    M(i)=getframe;
+    th=th+dtheta;
 end
 pause
 clf
